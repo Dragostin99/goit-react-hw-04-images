0bf5c7328c63dc160c7a65cce1e4243c78129b36
Dragostin99 <dragostin_lcs@yahoo.com>
2025-02-09 19:19:15 +0200

    BuugFix3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import Searchbar from './Searchbar.js/Searchbar';
 import fetchImages from './fetchImages/fetchImages';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Modal from './Modal/Modal';
-import Loader from './Loader/Loader';
 import LoadMore from './LoadMore/LoadMore';
 import ErrorDisplay from './errorDisplay/ErrorDisplay';
 
@@ -17,13 +16,11 @@ export const App = () => {
   const [maxPages, setMaxPages] = useState(null);
   const [largeImageUrl, setLargeImageUrl] = useState(null);
   const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setIsLoading(true);
         const { totalHits, hits } = await fetchImages(filter, pageNum);
         const imgMaxPages = Math.ceil(totalHits / perPage);
         setMaxPages(imgMaxPages);
@@ -33,9 +30,7 @@ export const App = () => {
       } catch (err) {
         console.log(err);
         setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
+      } 
     };
     if (filter) {
       fetchData();
@@ -63,7 +58,6 @@ export const App = () => {
           setModalIsVisible={setModalIsVisible}
         />
       )}
-      {isLoading && <Loader />}
       {maxPages > 1 && (
         <LoadMore
           setPageNum={setPageNum}
